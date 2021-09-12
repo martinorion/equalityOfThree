7c0ebd9ac19b90563a6f31ba3cbe0c86f00e2fff
MartinHolena <martinkiko18@gmail.com>
2021-09-12 13:32:33 +0200

    returns the amount of integers which are of equal value

--- equalityOfThree_/src/equalityOfThree.java
+++ equalityOfThree_/src/equalityOfThree.java
@@ -1,5 +1,18 @@
 public class equalityOfThree {
     public static void main(String[] args){
         System.out.println("Hello Buddy");
+        Numbers(1, 2, 1 );
+    }
+    static void Numbers(int a, int b, int c){
+        if(a == b && a != c || b == c && c != a || a == c && c != b){
+            System.out.println(2);
+        }
+        else if(a != b && a != c && b != c){
+            System.out.println(0);
+        }
+        else if (a == b && a == c){
+            System.out.println(3);
+        }
+
     }
 }
